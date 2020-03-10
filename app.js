e7a23f6c5a257acf0f5457841db562764cb13256
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-10 20:59:29 +0700

    test5

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
+var lovehomeRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/lovehome', lovehomeRouter);
 app.use('*', indexRouter);
 
 // catch 404 and forward to error handler
