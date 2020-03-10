513eda0479366621a0f5d84ea2e47187b995ceac
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-10 22:13:05 +0700

    test17

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var lovehomeRouter = require('./routes/lovehome');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, ''));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
