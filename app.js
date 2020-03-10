4affc6937bff74a8860e177c50ecd7197ee44128
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-10 20:57:00 +0700

    test4

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/lovehome', indexRouter);
 app.use('/users', usersRouter);
 app.use('*', indexRouter);
 
