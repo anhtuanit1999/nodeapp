0695ca3d3e73459b8d93e7cae5f1abdb356b7585
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-10 21:27:59 +0700

    test8

--- routes/lovehome.js
+++ routes/lovehome.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET lovehome page. */
 router.get('/', function (req, res, next) {
-  res.render('lovehome', { title: 'Express' });
+  res.render('lovehome');
 });
 
 module.exports = router;
