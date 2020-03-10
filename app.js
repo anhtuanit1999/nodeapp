af865a3219426ece6c38670e11ae20b050b286cd
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-10 22:06:14 +0700

    test15

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var lovehomeRouter = require('./routes/lovehome');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname+ 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
--- routes/lovehome.js
+++ routes/lovehome.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET lovehome page. */
 router.get('/', function (req, res, next) {
-  res.render('lovehome');
+  res.render('views/lovehome');
 });
 
 module.exports = router;
